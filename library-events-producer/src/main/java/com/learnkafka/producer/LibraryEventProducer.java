35bb8a8b005de8265292846eda3dad6b2fe7e175
Itamar Tafarello <tafarello.it@gmail.com>
2021-04-24 22:09:01 -0300

    Setup a Synchronous producer

--- library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -21,36 +25,58 @@ public class LibraryEventProducer {
     @Autowired
     ObjectMapper objectMapper;
 
+    //Asynchronous Call
     public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
         Integer key = libraryEvent.getLibraryEventId();
         String value = objectMapper.writeValueAsString(libraryEvent);
 
-        ListenableFuture<SendResult<Integer,String>> listenableFuture = kafkaTemplate.sendDefault(key,value);
+        ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, value);
 
         listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
             @Override
             public void onFailure(Throwable ex) {
-                handleFailure(key,value,ex);
+                handleFailure(key, value, ex);
             }
 
             @Override
             public void onSuccess(SendResult<Integer, String> result) {
                 handleSuccess(key, value, result);
             }
-
-            private void handleFailure(Integer key, String value, Throwable ex) {
-                log.error("Error Sending the message and the exception is {}", ex.getMessage());
-                try {
-                    throw ex;
-                } catch (Throwable throwable) {
-                    log.error("Error in OnFailure: {}", throwable.getMessage());
-                }
-            }
-            private void handleSuccess(Integer key, String value, SendResult<Integer,String> result) {
-                log.info("Message sent successfully for the key :{} and the value is {} , partition is {}",key, value, result.getRecordMetadata().partition());
-            }
         });
+    }
 
+    //Synchronous Call
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws
+            JsonProcessingException, ExecutionException, InterruptedException, TimeoutException, Exception {
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+
+        SendResult<Integer, String> sendResult = null;
+
+        try {
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1, TimeUnit.SECONDS);
+        } catch (ExecutionException | InterruptedException ex) {
+            log.error("ExecutionException/InterruptedException Sending the message and the exception is {}", ex.getMessage());
+        } catch (TimeoutException ex) {
+            log.error("TimeoutException Sending the message and the exception is {}", ex.getMessage());
+        } catch (Exception ex) {
+            log.error("Exception Sending the message and the exception is {}", ex.getMessage());
+        }
+        return sendResult;
+    }
+
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error Sending the message and the exception is {}", ex.getMessage());
+        try {
+            throw ex;
+        } catch (Throwable throwable) {
+            log.error("Error in OnFailure: {}", throwable.getMessage());
+        }
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message sent successfully for the key :{} and the value is {} , partition is {}", key, value, result.getRecordMetadata().partition());
     }
 
 }
