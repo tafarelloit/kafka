d77bd1aae0489d108935b8e5b41ab0bab75d0bec
Itamar Tafarello <tafarello.it@gmail.com>
2021-04-24 23:05:46 -0300

    Changed the LibraryEvent adding a new metadata enum LibraryEventType

--- library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -2,6 +2,7 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,19 +23,20 @@ public class LibraryEventsController {
     @PostMapping("/v1/libraryevent")
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws Exception {
         //Invoke Kafka Producer
-        log.info("Before sendLibraryEvent");
+        //log.info("Before sendLibraryEvent");
 
         //Asynchronous call
         //libraryEventProducer.sendLibraryEvent(libraryEvent);
 
         //Asynchronous call with Producer Record
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
         libraryEventProducer.sendLibraryEventWithProducerRecord(libraryEvent);
 
         //Synchronous call
         //SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
         //log.info("SendResult is{}",sendResult.toString());
 
-        log.info("after sendLibraryEvent");
+        //log.info("after sendLibraryEvent");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
